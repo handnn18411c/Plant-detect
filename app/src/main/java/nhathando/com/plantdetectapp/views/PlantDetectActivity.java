04a84edf062cd329d2693e081491f3d282160278
LENOVO <hoangloi2001@gmail.com>
2021-07-21 16:22:01 +0700

    Setup project

--- app/src/main/java/nhathando/com/plantdetectapp/views/PlantDetectActivity.java
+++ app/src/main/java/nhathando/com/plantdetectapp/views/PlantDetectActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import nhathando.com.plantdetectapp.R;
+
 public class PlantDetectActivity extends AppCompatActivity {
 
     @Override
